17cb3578b3bd7624d61ef0b2c557f8f2a82d7883
daniel-sj-hong <dan8492@gmail.com>
2021-06-13 14:58:55 -0700

    some weird things happened with my code and it reverting beyond a simply undo command of ctrl + z. I think I've fixed a good amount and restored it back to a good spot. committing what I got now.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@ var $views = document.querySelectorAll('.view');
 var $form = document.querySelector('.form');
 var $displayNickname = document.querySelector('.display-nickname');
 var $displayStoneText = document.querySelector('.display-stone-text');
+var $fireStone = document.querySelector('.fire-stone');
+var $waterStone = document.querySelector('.water-stone');
+var $thunderStone = document.querySelector('.thunder-stone');
+var $stoneSelection = document.querySelector('.stone-selection');
+var $evolutionText = document.querySelector('.evolution-text');
 
 $callButton.addEventListener('click', handleClick);
 $form.addEventListener('submit', handleSubmit);
+$fireStone.addEventListener('click', handleEvolution);
+$waterStone.addEventListener('click', handleEvolution);
+$thunderStone.addEventListener('click', handleEvolution);
 
 function getPokemonData(name) {
   var xhr = new XMLHttpRequest();
@@ -27,6 +35,25 @@ function getStones(name) {
     $stoneImg.setAttribute('src', xhr.response.sprites.default);
   });
   xhr.send();
+}
+
+function handleEvolution(event) {
+  if (event.target.matches('.fire-stone')) {
+    $stoneSelection.className = 'evolution-view hidden';
+    var flareon = getPokemonData('flareon');
+    $evolutionText.textContent = data.nickname + ' evolved into Flareon!';
+  }
+  if (event.target.matches('.water-stone')) {
+    $stoneSelection.className = 'evolution-view hidden';
+    var vaporeon = getPokemonData('vaporeon');
+    $evolutionText.textContent = data.nickname + ' evolved into Vaporeon!';
+  }
+  if (event.target.matches('.thunder-stone')) {
+    $stoneSelection.className = 'evolution-view hidden';
+    var jolteon = getPokemonData('jolteon');
+    $evolutionText.textContent = data.nickname + ' evolved into Jolteon!';
+  }
+  $displayNickname.className = 'hidden';
 }
 
 function handleSubmit(event) {
